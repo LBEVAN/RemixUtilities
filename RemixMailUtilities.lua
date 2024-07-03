4b4c2e501645908c55de73a2aecb3bd842317ac0
LBEVAN <LukeRJBevan@gmail.com>
2024-07-03 20:32:54 +0100

    Add ability to continually loot items from the mailbox
    * Loot function based on items ids so can be used for different item combinations.
    * Use frame update hook to loot items, 1 per frame.
    * Clean up frames when looting has finished and hook into mailbox closed event, which is now 'PLAYER_INTERACTION_MANAGER_FRAME_HIDE' post patch 10.0.

--- RemixMailUtilities.lua
+++ RemixMailUtilities.lua
@@ -1,5 +1,6 @@
 local addonName, addonTable = ...
 local RemixUtilities = addonTable.RemixUtilities
+local lootingFrame;
 
 --- Updates the info frame, setting the text based on item counts
 local function UpdateInfoFrame()
@@ -8,35 +9,41 @@ local function UpdateInfoFrame()
     RemixInfoFrame.expThreadsText:SetText(([[Exp Threads: %d]]):format(mailCounts[2]))
 end
 
---- Loots stat threads from the mailbox
-local function LootStatThreads()
-    print("looting stat threads")
-    local exit = false
-    for i = 1, GetInboxNumItems() do
-        local itemCount = select(8, GetInboxHeaderInfo(i))
-        if itemCount then
-            for j = 1, ATTACHMENTS_MAX_RECEIVE do
-                local currentItemID = select(2, GetInboxItem(i, j))
-                if RemixUtilities.StatThreadIds[currentItemID] then
-                    -- todo: implement coroutine to continually loot
-                    TakeInboxItem(i, j)
-                    exit = true
-                end
-                if exit then
-                    break;
-                end
-            end
-        end
-        if exit then
-            -- todo: remove, just temp to do 1 thread
-            break
-        end
+--- Stops any active looting process
+local function StopLooting()
+    if lootingFrame then
+        lootingFrame:SetScript("OnUpdate", nil)
+        lootingFrame = nil
+        print("RemixUtilities - Finished looting")
     end
 end
 
---- Loots experience threads from the mailbox
-local function LootExpThreads()
-    -- todo: implement
+--- Loots the specified items from the mailbox
+local function LootItems(itemIds)
+    lootingFrame = CreateFrame("Frame")
+    lootingFrame:SetScript("OnUpdate", function()
+        local hasLooted = false
+
+        for i = 1, GetInboxNumItems() do
+            local itemCount = select(8, GetInboxHeaderInfo(i))
+            if itemCount then
+                for j = 1, ATTACHMENTS_MAX_RECEIVE do
+                    local currentItemId = select(2, GetInboxItem(i, j))
+                    if itemIds[currentItemId] then
+                        TakeInboxItem(i, j)
+                        hasLooted = true
+                        -- return here to process one item per frame update
+                        return
+                    end
+                end
+            end
+        end
+
+        -- if no items were looted, stop the looting process
+        if not hasLooted then
+            StopLooting()
+        end
+    end)
 end
 
 --- Initialises the mail frames
@@ -57,7 +64,7 @@ local function InitFrames()
         LootStatThreadsButton:SetPoint("TOP", RemixMailFrame, "TOP", 0, -30)
         LootStatThreadsButton:SetText("Loot Stat Threads")
         RemixUtilities:ResizeButton(LootStatThreadsButton)
-        LootStatThreadsButton:SetScript("OnClick", function() LootStatThreads() end)
+        LootStatThreadsButton:SetScript("OnClick", function() LootItems(RemixUtilities.StatThreadIds) end)
     end
 
     -- create the 'loot xp threads' button
@@ -66,7 +73,7 @@ local function InitFrames()
         LootExpThreadsButton:SetPoint("TOP", LootStatThreadsButton, "TOP", 0, -25   )
         LootExpThreadsButton:SetText("Loot Exp Threads")
         RemixUtilities:ResizeButton(LootExpThreadsButton)
-        LootExpThreadsButton:SetScript("OnClick", function() LootExpThreads() end)
+        LootExpThreadsButton:SetScript("OnClick", function() LootItems(RemixUtilities.ExpThreadIds) end)
     end
 
     -- create the info frame
@@ -92,11 +99,14 @@ end
 local remixMailUtilities = CreateFrame("Frame")
 remixMailUtilities:RegisterEvent("MAIL_SHOW")
 remixMailUtilities:RegisterEvent("MAIL_INBOX_UPDATE")
+remixMailUtilities:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_HIDE")
 remixMailUtilities:SetScript("OnEvent", function(self, event, ...)
     if event == "MAIL_SHOW" then
         InitFrames()
     elseif event == "MAIL_INBOX_UPDATE" then
         UpdateInfoFrame()
+    elseif event == "PLAYER_INTERACTION_MANAGER_FRAME_HIDE" then
+        StopLooting()
     end
 end)
 
